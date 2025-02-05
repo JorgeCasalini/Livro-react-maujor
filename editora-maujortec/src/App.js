75220ef8271bd1890b330b6ca02ff95ac9bed040
jorgeCasalini <joshcasalini@gmail.com>
2025-02-05 14:44:31 -0300

    Creates the application's footer and ends the first part with the static website

--- editora-maujortec/src/App.js
+++ editora-maujortec/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Topo from './components/Topo.js';
+import Principal from './components/Principal.js';
+import Rodape from './components/Rodape.js';
 
 const App = () => {
   return (
+    <>
       <Topo />
+      <Principal />
+      <Rodape />
+    </>
   );
 }
 
