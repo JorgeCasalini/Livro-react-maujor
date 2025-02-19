6ff89f920c0e83541a3d1ac7f7c4cbeced004141
jorgeCasalini <joshcasalini@gmail.com>
2025-02-19 17:14:30 -0300

    Originates the 'Livros.js' component that is used to display the page with details of each book

--- editora-maujortec/src/App.js
+++ editora-maujortec/src/App.js
@@ -4,29 +4,57 @@ import Topo from './components/Topo.js'
 import Home from './components/Home.js';
 import Catalogo from './components/Catalogo.js';
 import Frontend from './components/Frontend.js';
+import Programacao from './components/Programacao.js';
+import Design from './components/Design.js';
 import NotFound from './components/NotFound.js';
 import Rodape from './components/Rodape.js';
 import "./index.css"
-import Programacao from './components/Programacao.js';
-import Design from './components/Design.js';
+import Livro from './components/Livro';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    livros: []
+  };
+
+  async componentDidMount() {
+    try {
+      const { data: livros } = await axios.get("/api/todosOsLivros.json");
+      this.setState({ livros });
+    } catch (error) {
+      console.log(error);
+      document
+        .querySelectorAll(".principal")[0]
+        .insertAdjacentHTML(
+          "beforeend",
+          "<p class='erro'>Mensagem de erro</p>"
+        );
+    }
+
+  }
+
   render() {
     return (
       <Router>
-        <>
-          <Topo />
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/frontend" element={<Frontend />} />
-            <Route exact path="/programacao" element={<Programacao />} />
-            <Route exact path="/design" element={<Design />} />
-            <Route exact path="/catalogo" element={<Catalogo />} />
-            <Route element={<NotFound />}/>
-          </Routes>
-        </>
+        <Topo />
+        <Routes>
+          <Route exact path="/" element={<Home livros={this.state.livros} />} />
+          <Route exact path="/frontend" element={<Frontend livros={this.state.livros} />} />
+          <Route exact path="/programacao" element={<Programacao livros={this.state.livros} />} />
+          <Route exact path="/design" element={<Design livros={this.state.livros} />} />
+          <Route exact path="/catalogo" element={<Catalogo livros={this.state.livros} />} />
+          <Route path="/livro/:livroSlug" element={props => {
+            const livro = this.state.livros.find(
+              livro => livro.slug === props.match.params.livroSlug);
+            if (livro) return <Livro livro={livro} />;
+            else return <NotFound />;
+          }}
+          />
+          <Route element={<NotFound />} />
+        </Routes>
+        <Rodape />
       </Router>
-    )
+    );
   }
 }
 
