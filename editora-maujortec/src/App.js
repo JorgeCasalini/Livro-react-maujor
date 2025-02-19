a83adaa33588d675d30bcc5c6f1ad29b6a80dffb
jorgeCasalini <joshcasalini@gmail.com>
2025-02-19 16:02:11 -0300

    Design the website routes using the 'React Router' library and make the navigation menu functional

--- editora-maujortec/src/App.js
+++ editora-maujortec/src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Topo from './components/Topo.js';
-import Principal from './components/Principal.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Topo from './components/Topo.js'
+import Home from './components/Home.js';
+import Catalogo from './components/Catalogo.js';
+import Frontend from './components/Frontend.js';
+import NotFound from './components/NotFound.js';
 import Rodape from './components/Rodape.js';
+import "./index.css"
+import Programacao from './components/Programacao.js';
+import Design from './components/Design.js';
 
-const App = () => {
-  return (
-    <>
-      <Topo />
-      <Principal />
-      <Rodape />
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <>
+          <Topo />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/frontend" element={<Frontend />} />
+            <Route exact path="/programacao" element={<Programacao />} />
+            <Route exact path="/design" element={<Design />} />
+            <Route exact path="/catalogo" element={<Catalogo />} />
+            <Route element={<NotFound />}/>
+          </Routes>
+        </>
+      </Router>
+    )
+  }
 }
 
 export default App;
--- editora-maujortec/src/components/Navegacao.js
+++ editora-maujortec/src/components/Navegacao.js
@@ -5,23 +5,25 @@ let linkCorrente = {
     color: "#027399"
 }
 const Navegacao = () => {
-    <ul>
-        <li>
-            <NavLink exact activeStyle={linkCorrente} to="/">Home</NavLink>
-        </li>
-        <li>
-            <NavLink exact activeStyle={linkCorrente} to="/frontend">Frontend</NavLink>
-        </li>
-        <li>
-            <NavLink exact activeStyle={linkCorrente} to="/programacao">Programação</NavLink>
-        </li>
-        <li>
-            <NavLink exact activeStyle={linkCorrente} to="/design">Design</NavLink>
-        </li>
-        <li>
-            <NavLink exact activeStyle={linkCorrente} to="/catalogo">Catálogo</NavLink>
-        </li>
-    </ul>
+    return (
+        <ul>
+            <li>
+                <NavLink exact activeStyle={linkCorrente} to="/">Home</NavLink>
+            </li>
+            <li>
+                <NavLink exact activeStyle={linkCorrente} to="/frontend">Frontend</NavLink>
+            </li>
+            <li>
+                <NavLink exact activeStyle={linkCorrente} to="/programacao">Programação</NavLink>
+            </li>
+            <li>
+                <NavLink exact activeStyle={linkCorrente} to="/design">Design</NavLink>
+            </li>
+            <li>
+                <NavLink exact activeStyle={linkCorrente} to="/catalogo">Catálogo</NavLink>
+            </li>
+        </ul>
+    )
 };
 
 export default Navegacao;
