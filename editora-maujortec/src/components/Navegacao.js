fe31583d305b8640de0ad971717d988a108003c0
jorgeCasalini <joshcasalini@gmail.com>
2025-02-20 14:58:28 -0300

    Update the 'Navegacao.js' folder to the latest method of navigating between pages

--- editora-maujortec/src/components/Navegacao.js
+++ editora-maujortec/src/components/Navegacao.js
@@ -1,29 +1,51 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-let linkCorrente = {
-    color: "#027399"
-}
 const Navegacao = () => {
-    return (
-        <ul>
-            <li>
-                <NavLink exact activeStyle={linkCorrente} to="/">Home</NavLink>
-            </li>
-            <li>
-                <NavLink exact activeStyle={linkCorrente} to="/frontend">Frontend</NavLink>
-            </li>
-            <li>
-                <NavLink exact activeStyle={linkCorrente} to="/programacao">Programação</NavLink>
-            </li>
-            <li>
-                <NavLink exact activeStyle={linkCorrente} to="/design">Design</NavLink>
-            </li>
-            <li>
-                <NavLink exact activeStyle={linkCorrente} to="/catalogo">Catálogo</NavLink>
-            </li>
-        </ul>
-    )
+  return (
+    <ul>
+      <li>
+        <NavLink 
+          to="/" 
+          style={({ isActive }) => (isActive ? { color: "#027399" } : undefined)}
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink 
+          to="/frontend" 
+          style={({ isActive }) => (isActive ? { color: "#027399" } : undefined)}
+        >
+          Frontend
+        </NavLink>
+      </li>
+      <li>
+        <NavLink 
+          to="/programacao" 
+          style={({ isActive }) => (isActive ? { color: "#027399" } : undefined)}
+        >
+          Programação
+        </NavLink>
+      </li>
+      <li>
+        <NavLink 
+          to="/design" 
+          style={({ isActive }) => (isActive ? { color: "#027399" } : undefined)}
+        >
+          Design
+        </NavLink>
+      </li>
+      <li>
+        <NavLink 
+          to="/catalogo" 
+          style={({ isActive }) => (isActive ? { color: "#027399" } : undefined)}
+        >
+          Catálogo
+        </NavLink>
+      </li>
+    </ul>
+  );
 };
 
 export default Navegacao;
